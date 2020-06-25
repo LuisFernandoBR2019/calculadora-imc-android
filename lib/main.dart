3edfc6cbfd9242875507780445aec47b0e0863a3
Robson Santos <robsonsantos@fai-mg.br>
2020-06-25 10:19:49 -0300

    Ajuste comentario

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Calculadora de IMC"),
+          title: Text("Calculadora de IMC 2"),
           centerTitle: true,
           backgroundColor: Colors.green,
           actions: <Widget>[
